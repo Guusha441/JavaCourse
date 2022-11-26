e38496cce25a5b3ae260ed738351c2254b692e97
Guusha441 <66376157+Guusha441@users.noreply.github.com>
2022-11-26 23:28:30 +0100

    Created new method returning Sum of two digits

--- src/JavaGuled/Main.java
+++ src/JavaGuled/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("hello World");
 	    System.out.println(2+2);
+        int Sum =main(2,5);
+        System.out.println(Sum);
+    }
+    static int main(int m, int g){
+        int result = m +g;
+        return result;
     }
 }
