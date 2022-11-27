c21056c66bf90346104eee8bb6bc68a72c98ae8b
Guusha441 <66376157+Guusha441@users.noreply.github.com>
2022-11-27 01:34:18 +0100

    main ayaan ka commit gareeyay

--- src/JavaGuled/Main.java
+++ src/JavaGuled/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	    System.out.println(2+2);
         int Sum =main(2,5);
         System.out.println(Sum);
-     
+        System.out.println("main ayaan ka commit gareeyay");
     }
     static int main(int m, int g){
         int result = m +g;
